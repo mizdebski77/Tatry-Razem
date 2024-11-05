f428c62adb3a0cb2ac4d65556651a9f20d27f0c3
Marcin Izdebski <mizdebski123@gmail.com>
2024-11-05 10:48:52 +0100

    Images to footer added

--- src/app/common/Footer/footer.tsx
+++ src/app/common/Footer/footer.tsx
@@ -1,8 +1,10 @@
 'use client'
 
 import React from 'react';
-import { AppImage, AppSection, AppText, AppTextTitle, AppTextWrapper, Wrapper } from './styledFooter';
+import { AppImage, AppSection, AppStore, AppStoresWrapper, AppText, AppTextTitle, AppTextWrapper, Wrapper } from './styledFooter';
 import app from '../Images/FooterImages/phone.svg';
+import google from '../Images/FooterImages/googleStore.svg';
+import store from '../Images/FooterImages/appStore.svg';
 
 export const Footer = () => {
     return (
@@ -12,6 +14,11 @@ export const Footer = () => {
                 <AppTextWrapper>
                     <AppTextTitle>Już wkrótce aplikacja dostępna na telefonie</AppTextTitle>
                     <AppText>Aplikacja dostępna będzie w najpopularniejszych sklepach!</AppText>
+                    <AppStoresWrapper>
+                        <AppStore src={google.src} alt='Google Store' />
+                        <AppStore src={store.src} alt='App Store' />
+
+                    </AppStoresWrapper>
                 </AppTextWrapper>
             </AppSection>
             xd
--- src/app/common/Footer/styledFooter.ts
+++ src/app/common/Footer/styledFooter.ts
@@ -6,7 +6,7 @@ export const Wrapper = styled.footer`
     background-position: center;
     background-size: cover;
     padding: 80px;
-    box-shadow: inset 0px -100px 82px 41px #141414;
+    box-shadow: inset 0px -100px 182px 141px #141414;
 `;
 
 export const AppSection = styled.div`
@@ -22,17 +22,22 @@ export const AppSection = styled.div`
 export const AppImage = styled.img`
     max-width: 280px;
     width: 100%;
+
 `;
 
 export const AppTextWrapper = styled.div`
     max-width: 800px;
     display: grid;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    gap: 20px;
 `;
 
 export const AppTextTitle = styled.text`
     color: white;
     font-size: 64px;
-    text-align: center;
+
 `;
 
 export const AppText = styled.p`
@@ -40,15 +45,16 @@ export const AppText = styled.p`
     color: white;
     font-weight: lighter;
     opacity: 0.9;
-    text-align: center;
 `;
 
 export const AppStoresWrapper = styled.div`
-
+    display: flex;
+    justify-content: center;
+    gap: 40px;
 `;
 
 export const AppStore = styled.img`
-
+    max-width: 200px;
 `;
 
 export const Content = styled.div`

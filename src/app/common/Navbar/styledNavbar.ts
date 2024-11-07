87174fdfef2fcf17af8836adc78d2c5478b21105
Marcin Izdebski <mizdebski123@gmail.com>
2024-11-07 12:33:31 +0100

    Navbar wrapper style changed

--- src/app/common/Navbar/styledNavbar.ts
+++ src/app/common/Navbar/styledNavbar.ts
@@ -1,12 +1,13 @@
 import { styled } from "styled-components";
 
 export const Wrapper = styled.nav`
-    padding: 32px;
+    padding: 28px;
     position: fixed;
     z-index: 10;
     top: 0;
     width: 100%;
-    backdrop-filter: blur(10px);
+    background: rgba(0, 0, 0, 0.1);
+    backdrop-filter: blur(4px);
 `;
 
 export const Link = styled.a`
@@ -16,6 +17,12 @@ export const Link = styled.a`
     gap: 12px;
     font-size: 18px;
     color: white;
+    text-decoration: none;
+    transition: 0.3s;
+
+    &:hover {
+        transform: translateY(-4px);
+    }
 `;
 
 export const LinkImage = styled.img`

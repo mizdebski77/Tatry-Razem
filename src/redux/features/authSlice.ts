a49a61b8c0bcbc12397e1d7fe8c17746633a3ae6
Marcin Izdebski <mizdebski123@gmail.com>
2024-11-20 14:36:27 +0100

    Auth slice added

---
+++ src/redux/features/authSlice.ts
@@ -0,0 +1,27 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit/react"
+
+type InitailState = {
+
+};
+
+type AuthState = {
+
+}
+
+const initialState = {
+    value: {
+
+    }
+}
+
+export const auth = createSlice({
+    name: 'auth',
+    initialState,
+    reducers: {
+
+    }
+})
+
+
+export const { } = auth.actions;
+export default auth.reducer;

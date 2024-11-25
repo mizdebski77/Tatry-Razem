dc5b2f446734fe17ae6ccece80f57d45f58b48aa
Marcin Izdebski <mizdebski123@gmail.com>
2024-11-25 14:18:52 +0100

    Profile data added

--- src/app/(features)/(auth)/Profile/page.tsx
+++ src/app/(features)/(auth)/Profile/page.tsx
@@ -1,10 +1,12 @@
 'use client';
 import { createClient } from '@/app/core/supabase/client';
-import { Header, HeaderTitle, Wrapper } from './styledProfile';
+import { Container, Tile, TileData, TileLInksWrapper, TileSpan, TileTitle, Title, UserImage, Wrapper } from './styledProfile';
 import { useQuery } from '@tanstack/react-query';
 import { useState } from 'react';
 import { Loader } from '@/app/common/Loader/loader';
 import { FetchError } from '@/app/common/Error/error';
+import { UserRound } from 'lucide-react';
+import { Button } from '@/app/common/UI/UI';
 
 export default function Profile() {
     const [file, setFile] = useState('');
@@ -34,12 +36,32 @@ export default function Profile() {
 
     return (
         <Wrapper>
-            <Header>
-                <HeaderTitle>
-                    Profil użytkownika {user?.user_metadata?.name || 'Nieznany'}
-                </HeaderTitle>
-            </Header>
-            <input onChange={(e) => setFile(e.target.value)} type="file" />
-        </Wrapper>
+            <Title>
+                Wtiaj {user?.user_metadata?.name}!
+            </Title>
+            <Container>
+                <Tile>
+                    <UserImage>
+                        <UserRound style={{ color: 'white', width: '240px', height: '240px' }} />
+                    </UserImage>
+                    <TileTitle>Podstawowe dane</TileTitle>
+                    <TileSpan>Bio: Lorem ipsum, dolor sit amet consectetur adipisicing elit. Totam ratione est mollitia dignissimos, ad officiis, vitae nam delectus adipisci corporis modi ducimus sunt eligendi cum tempore ab fuga quod facere?</TileSpan>
+                    <TileSpan>Imię i Nazwisko: <TileData>{user?.user_metadata.name} {user?.user_metadata.surname}</TileData> </TileSpan>
+                    <TileSpan>E-Mail: <TileData>{user?.email}</TileData> </TileSpan>
+                    <TileSpan>Miejscowość: <TileData>Kraków</TileData></TileSpan>
+                    <TileLInksWrapper>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M7.9 20A9 9 0 1 0 4 16.1L2 22Z" /></svg>                    <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M2.5 17a24.12 24.12 0 0 1 0-10 2 2 0 0 1 1.4-1.4 49.56 49.56 0 0 1 16.2 0A2 2 0 0 1 21.5 7a24.12 24.12 0 0 1 0 10 2 2 0 0 1-1.4 1.4 49.55 49.55 0 0 1-16.2 0A2 2 0 0 1 2.5 17" /><path d="m10 15 5-3-5-3z" /></svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z" /></svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><rect width="20" height="20" x="2" y="2" rx="5" ry="5" /><path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z" /><line x1="17.5" x2="17.51" y1="6.5" y2="6.5" /></svg>
+                    </TileLInksWrapper>
+                    <Button
+                        $background='blue'
+                        onClick={() => setFile('hej')}
+                        text='Edytuj dane'
+                        disabled={false}
+                    />
+                </Tile>
+            </Container>
+        </Wrapper >
     );
 }
--- src/app/(features)/(auth)/Profile/styledProfile.ts
+++ src/app/(features)/(auth)/Profile/styledProfile.ts
@@ -7,35 +7,71 @@ import { theme } from "@/app/core/StyledComponents/theme";
 
 export const Wrapper = styled.section`
     min-height: 100vh;
-`;
-
-export const Header = styled.header`
-    background-image: url(${headerBackground.src});
     background-position: center;
     background-size: cover;
-    display: flex;
-    min-height: 40vh;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    padding: 20px;
-    gap: 20px;
-
-    /* @media (max-width: ${theme.mediaQuery.screen}) {
-        padding: 120px 20px;
-        justify-content: start;
-    }; */
-
+    padding: 160px 80px;
 `;
 
-export const HeaderTitle = styled.h1`
+export const Title = styled.h1`
     color: white;
     font-size: 64px;
     font-weight: normal;
     text-align: center;
     letter-spacing: 5px;
+    color: black;
+    font-weight: bold;
 `;
 
 export const Container = styled.article`
+    display: flex;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    padding: 40px;
+`;
 
+export const Tile = styled.div`
+    padding: 80px 40px;
+    background: white;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+    border-radius: 20px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 32px;
+    text-align: center;
+`;
+
+export const UserImage = styled.div`
+    background: ${theme.palette.blue};
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 100%;
+    max-width: fit-content;
+    padding: 12px;
+    margin: 0 auto;
+`;
+
+export const TileTitle = styled.p`
+    font-size: 40px;
+`;
+
+export const TileSpan = styled.p`
+    font-size: 20px;
+    padding: 12px;
+    width: 100%;
+    background: #f3f3f3;
+`;
+
+export const TileData = styled.span`
+    font-weight: bold;
+    color: ${theme.palette.blue};
+`;
+
+export const TileLInksWrapper = styled.div`
+    display: flex;
+    justify-content: space-evenly;
+    max-width: 320px;
+    width: 100%;
 `;
